3e6a20e1dd3ae954341d6bef9925bb2797790ab4
OhnishiWataru <ohnishi.wataru.business@gmail.com>
2019-03-07 20:06:34 +0900

    PHP14kadai

---
+++ PHP14/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+  Route::post('news/create', 'Admin\NewsController@create');
+
+  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+  Route::post('profile/create', 'Admin\ProfileController@create');
+  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+  Route::post('profile/edit', 'Admin\ProfileController@update');
+});
+
+Route::get('XXX', 'AAAController@bbb');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
