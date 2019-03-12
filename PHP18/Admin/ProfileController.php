434a852754acdf82752d99de91ce8f705f4d70bd
OhnishiWataru <ohnishi.wataru.business@gmail.com>
2019-03-12 20:24:05 +0900

    PHP18kadai

---
+++ PHP18/Admin/ProfileController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Profile;
+
+use App\ProfileHistory;
+
+use Carbon\Carbon;
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+      return view('admin.profile.create');
+    }
+
+    public function create(Request $request)
+    {
+
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+      unset($form['_token']);
+
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/create');
+    }
+
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = Profile::where('title', $cond_title)->get();
+        } else {
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+
+    public function edit(Request $request)
+    {
+        $profile = Profile::find($request->id);
+
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+
+    public function update(Request $request)
+    {
+      $this->validate($request, Profile::$rules);
+      $profile = Profile::find($request->id);
+      $profile_form = $request->all();
+
+      unset($profile_form['_token']);
+      $profile->fill($profile_form)->save();
+
+      $profilehistory = new ProfileHistory;
+      $profilehistory->profile_id = $profile->id;
+      $profilehistory->edited_at =Carbon::now();
+      $profilehistory->save();
+
+      return redirect('admin/profile/');
+    }
+
+    public function delete(Request $request)
+    {
+        $profile = Profile::find($request->id);
+        $profile->delete();
+
+        return redirect('admin/profile/');
+    }
+}
